bd5150c64629313586f7fbed0c1d71875dba6db2
julian-saleix <julian.saleix@gmail.com>
2022-11-16 16:09:16 +0100

    Updating models with __str__ fns & implemented them in admin

--- code/library/admin.py
+++ code/library/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book, Library, Book_Reference, Genre
+
+admin.site.register(Book)
+admin.site.register(Book_Reference)
+admin.site.register(Library)
+admin.site.register(Genre)
--- code/library/models.py
+++ code/library/models.py
@@ -7,7 +7,8 @@ class Library(models.Model):
     zipcode = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return "{self.name} - {self.city}".format(self=self)
+
 class Book_Reference(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
@@ -16,16 +17,25 @@ class Book_Reference(models.Model):
     edition = models.CharField(max_length=200)
     collection = models.CharField(max_length=200)
     synopsis = models.TextField()
+    def __str__(self):
+        return f"'{self.title}' by {self.author} ({self.year})"
 
 class Book(models.Model):
     reference = models.ForeignKey(Book_Reference, on_delete=models.CASCADE)
     library = models.ForeignKey(Library, on_delete=models.CASCADE)
     stock = models.IntegerField(default=0)
+    genre = models.ManyToManyField('Genre')
+    def __str__(self):
+        return f"{self.reference.__str__()} in {self.library.name}"
 
 class Genre(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    def __str__(self):
+        return self.name
 
-class Book_Genre(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+# class Book_Genre(models.Model):
+#     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+#     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+#     def __str__(self):
+#         return "{self.name} - {self.city}".format(self=self)
