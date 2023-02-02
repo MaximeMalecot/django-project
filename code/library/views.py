6bcdbe05818913dc4c40d1291f406dae6cdefc33
julian-saleix <julian.saleix@gmail.com>
2023-02-02 19:46:42 +0100

    Adding base template/static folders

--- code/library/views.py
+++ code/library/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Book
 
 def index(request):
-    return render(request, 'index.html')
+    books = Book.objects.all()
+    return render(request, 'library/index.html', {'books': books})
