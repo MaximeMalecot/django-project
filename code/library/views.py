2ce693ecc36e8030b8a591d71fb5c1c7349fda79
julian-saleix <julian.saleix@gmail.com>
2023-02-04 17:56:08 +0100

    book reference page added

--- code/library/views.py
+++ code/library/views.py
@@ -8,5 +8,8 @@ def index(request):
     return render(request, 'library/index.html', {'books': books})
 
 def book_reference(request, book_id):
-    book = Book.objects.get(pk=book_id)
-    return render(request, 'library/book.html', {'book': book})
+    book = Book_Reference.objects.get(pk=book_id)
+    if book is None:
+        raise Http404("Book does not exist")
+    availabilities = Book.objects.filter(reference=book, stock__gt=0)
+    return render(request, 'library/book.html', {'book': book, 'availabilities': availabilities})
